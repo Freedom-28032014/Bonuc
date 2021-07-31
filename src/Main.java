80506db0787771e39ea999372981b8a2d65c74da
Tatiana Lukina <tanyalykina57@gmail.com>
2021-07-31 06:34:15 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
         int balance = 100;
         int deposit = 1100;
         int bonus;
-        if (deposit>1000) {
-            bonus = deposit/100;
-        }else {
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
             bonus = 0;
         }
-    int totalBalance = balance + deposit + bonus;
-        System.out.println("Current Balance: "+totalBalance);
-        System.out.println("Bonus: "+bonus);
+        int totalBalance = balance + deposit + bonus;
+        System.out.println("Current Balance: " + totalBalance);
+        System.out.println("Bonus: " + bonus);
 
 
     }
